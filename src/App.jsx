1cd1095dcce7b71f055ece60eab9e973b8a55b1f
silmolin <silmolin8@gmail.com>
2024-04-30 12:53:41 -0300

    cambioss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./app.scss";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
-import Test from "./components/Test";
+import Parallax from "./components/parallax/Parallax";
+//import Test from "./components/Test";
 
 const App = () => {
   return <div>
@@ -9,13 +10,13 @@ const App = () => {
       <Navbar/>
       <Hero/>
     </section>
-    <section id="Services">Parallax</section>
-    <section>Servicios</section>
-    <section id="Portfolio">Parallax</section>
+    <section id="Services"><Parallax type="services"/></section>
+    <section>Services </section>
+    <section id="Portfolio"><Parallax type="portfolio"/></section>
     <section>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
-    <section id="Contacto">Contacto</section>
+    <section id="Contact">Contact</section>
     {/* <Test/> */}
   </div>;
 };
